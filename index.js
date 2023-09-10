aaf234ca590d4ea7a8f51a6eaeeee1312ab88300
yichi <zoechen723@gmail.com>
2023-09-11 00:05:40 +0800

    socket server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server,{
+    cors: {
+        origin: "http://localhost:5173"
+      }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3310, () => {
+  console.log('socket running');
+});
