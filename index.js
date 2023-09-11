511ea99fa8e48d0c596ef3cd29dd7793a09c1ec8
yichi <zoechen723@gmail.com>
2023-09-11 10:58:01 +0800

    cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server,{
     cors: {
-        origin: "http://localhost:5173"
+        origin: "https://starlit-tapioca-f08207.netlify.app"//"http://localhost:5173"
       }
 });
 
